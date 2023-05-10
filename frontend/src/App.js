4c9dc05adff376e5338d6e3de2e3fb1df7a19163
gekoparty <gekoparty@hotmail.com>
2023-05-10 19:31:55 +0200

    Add and delete categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 
 import "./App.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { Outlet, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import PermanentDrawerLeft from "./components/NavBar/PermanentDrawerLeft";
