5e0e11e3937da25c854aff7dbb55dca038055ad2
gekoparty <gekoparty@hotmail.com>
2023-06-21 22:42:53 +0200

    added some error handling, buggy

--- frontend/src/components/commons/BrandDialogs/AddBrandDialog.js
+++ frontend/src/components/commons/BrandDialogs/AddBrandDialog.js
@@ -1,15 +1,23 @@
 // AddBrandDialog.js
-import React, { useState, useContext } from "react";
-import { Button, TextField } from "@mui/material";
+import React, { useState, useContext, useEffect } from "react";
+import { Button, TextField,Typography } from "@mui/material";
 
 import { StoreContext } from "../../../Store/Store";
 import BasicDialog from "../BasicDialog/BasicDialog";
 import useCustomHttp from "../../../hooks/useHttp";
 
-const AddBrandDialog = ({ open, onClose, onAdd }) => {
-  const { addData } = useCustomHttp("/api/brands");
+const AddBrandDialog = ({ open, onClose, onAdd, }) => {
+  const { addData, error } = useCustomHttp("/api/brands");
   const { dispatch } = useContext(StoreContext);
   const [brandName, setBrandName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect(() => {
+    if (!open) {
+      setBrandName("");
+      setErrorMessage("");
+    }
+  }, [open]);
 
   const handleAddBrand = async () => {
     // Perform validation, create a new brand object, and add it to the state
@@ -18,9 +26,15 @@ const AddBrandDialog = ({ open, onClose, onAdd }) => {
 
     try {
       const response = await addData("/api/brands", "POST", newBrand);
-
+      
       if (response.error) {
-        console.log("Error adding brand:", response.error);
+        console.log(error)
+        if (error.response.data.message === "Brand already exists") {
+          setErrorMessage("Dette merket eksisterer allerede");
+        } else {
+          setErrorMessage("Feil med lagring av data, prøv igjen");
+        }
+        console.log("Error adding brand:", error);
       } else {
         console.log("data coming from hook", response);
         const { _id, name } = response.data; // Destructure the desired fields from response.data
@@ -29,26 +43,37 @@ const AddBrandDialog = ({ open, onClose, onAdd }) => {
         onAdd(newBrand);
         setBrandName("");
         onClose();
+        setErrorMessage("");
       }
     } catch (fetchError) {
+      setErrorMessage("Feil med lagring av data, prøv igjen")
       console.log("Error adding brand:", fetchError);
     }
   };
 
   return (
+    <>
     <BasicDialog
       open={open}
       onClose={onClose}
-      dialogTitle="Add Brand"
-      confirmButton={<Button onClick={handleAddBrand}>Add</Button>}
-      cancelButton={<Button onClick={onClose}>Cancel</Button>}
+      dialogTitle="Nytt Merke"
+      confirmButton={<Button onClick={handleAddBrand}>Lagre</Button>}
+      cancelButton={<Button onClick={onClose}>Kanseler</Button>}
     >
-      <TextField
-        label="Brand Name"
+      <TextField sx={{marginTop: 1}}
+        label="Merke"
         value={brandName}
         onChange={(e) => setBrandName(e.target.value)}
       />
+      { errorMessage && (
+    <Typography sx={{marginTop: 1}} variant="body1" color="error">
+      {errorMessage}
+    </Typography>
+    )}
+
     </BasicDialog>
+    
+    </>
   );
 };
 
