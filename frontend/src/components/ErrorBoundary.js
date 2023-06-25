c789b5254d25d00141cd96f0ca03ed455703c481
gekoparty <gekoparty@hotmail.com>
2023-06-25 23:50:16 +0200

    Global error

---
+++ frontend/src/components/ErrorBoundary.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ErrorHandling from "./commons/ErrorHandling/ErrorHandling";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can log the error or send it to an error tracking service
+    console.error("Error caught in ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <ErrorHandling
+          resource="global" // Set a global resource name for the ErrorHandling component
+        />
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
