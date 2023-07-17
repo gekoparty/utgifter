a512b38e342cda5152b866957e8f975944873b3f
gekoparty <gekoparty@hotmail.com>
2023-07-17 19:29:50 +0200

    modifications errorhandling editbrand

--- frontend/src/components/commons/BrandDialogs/EditBrandDialog.js
+++ frontend/src/components/commons/BrandDialogs/EditBrandDialog.js
@@ -1,10 +1,11 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { Button, TextField } from "@mui/material";
 import PropTypes from "prop-types";
 import useCustomHttp from "../../../hooks/useHttp";
 import BasicDialog from "../BasicDialog/BasicDialog";
 import { StoreContext } from "../../../Store/Store";
 import { addBrandValidationSchema } from "../../../validation/validationSchema";
+import ErrorHandling from "../ErrorHandling/ErrorHandling";
 
 const EditBrandDialog = ({
   open,
@@ -14,49 +15,116 @@ const EditBrandDialog = ({
   onUpdateSuccess,
   onUpdateFailure,
 }) => {
+  const [brandName, setBrandName] = useState(selectedBrand?.name);
 
-  const { sendRequest } = useCustomHttp("/api/brands");
-  const [updateLoading, setUpdateLoading] = useState(false);
-  const [brandName, setBrandName] = useState(selectedBrand?.name)
-  const { dispatch } = useContext(StoreContext);
+  const { sendRequest, loading } = useCustomHttp("/api/brands");
+  const { dispatch, state } = useContext(StoreContext);
+
+  const resetValidationErrors = useCallback(() => {
+    dispatch({ type: "RESET_VALIDATION_ERRORS", resource: "brands" });
+  }, [dispatch]);
+
+  const resetServerError = useCallback(() => {
+    dispatch({ type: "RESET_ERROR", resource: "brands" });
+  }, [dispatch]);
+
+  const resetFormAndErrors = useCallback(() => {
+    setBrandName("");
+    dispatch({ type: "RESET_ERROR", resource: "brands" });
+    resetValidationErrors();
+  }, [dispatch, resetValidationErrors]);
+
+  useEffect(() => {
+    if (!open) {
+      resetFormAndErrors();
+    }
+  }, [open, resetFormAndErrors]);
 
   useEffect(() => {
     setBrandName(selectedBrand?.name || "");
-  }, [selectedBrand]);
- 
+    resetValidationErrors();
+  }, [selectedBrand, resetValidationErrors]);
+
   const handleUpdateBrand = async () => {
-    await addBrandValidationSchema.validate({ brandName });
-    
-    setUpdateLoading(true);
+    if (typeof brandName !== "string" || brandName.trim().length === 0) {
+      return; // Prevent submitting invalid or empty brand name
+    }
 
     try {
-        const updatedBrand = { ...selectedBrand, name: brandName };
-      const response = await sendRequest(
+      await addBrandValidationSchema.validate({ brandName });
+      resetValidationErrors();
+    } catch (validationError) {
+      setValidationErrors(validationError.message);
+      return;
+    }
+
+    try {
+      const updatedBrand = { ...selectedBrand, name: brandName };
+      const { data, error: updateDataError } = await sendRequest(
         `/api/brands/${selectedBrand?._id}`,
         "PUT",
         updatedBrand
       );
 
-      if (response.error) {
-        console.log("error updating brand", response.error);
+      if (updateDataError) {
+        console.log("Error updating brand", updateDataError);
+        dispatch({
+          type: "SET_ERROR",
+          error: updateDataError,
+          resource: "brands",
+          showError: true,
+        });
         onUpdateFailure(selectedBrand);
       } else {
-        console.log("Brand updated succesfully", response.data);
-        onUpdateSuccess(response.data);
+        console.log("Brand updated successfully", data);
+        onUpdateSuccess(data);
         dispatch({
           type: "UPDATE_ITEM",
           resource: "brands",
-          payload: response.data,
+          payload: data,
         });
+        resetValidationErrors();
+        resetFormAndErrors();
+        onClose();
       }
-    } catch (error) {
-      console.log("Error updating brand", error);
+    } catch (fetchError) {
+      setError(fetchError);
       onUpdateFailure(selectedBrand);
     } finally {
-      setUpdateLoading(false);
-      onClose();
     }
   };
+
+  const setValidationErrors = (errorMessage) => {
+    dispatch({
+      type: "SET_VALIDATION_ERRORS",
+      resource: "brands",
+      validationErrors: { brandName: errorMessage },
+      showError: true,
+    });
+  };
+
+  const setError = (fetchError) => {
+    dispatch({
+      type: "SET_ERROR",
+      error: fetchError,
+      resource: "/api/brands",
+      showError: true,
+    });
+  };
+
+  const displayError = state.error?.brands;
+  const validationError = state.validationErrors?.brands?.brandName;
+
+  console.log(displayError);
+
+  const isFormValid = () => {
+    return (
+      typeof brandName === "string" &&
+      brandName.trim().length > 0 &&
+      !validationError
+    );
+  };
+
   return (
     <BasicDialog
       open={open}
@@ -64,31 +132,43 @@ const EditBrandDialog = ({
       dialogTitle={dialogTitle}
       onConfirm={handleUpdateBrand}
       cancelButton={
-        <Button onClick={onClose} disabled={updateLoading}>
+        <Button onClick={onClose} disabled={loading}>
           Avbryt
         </Button>
       }
       confirmButton={
-        <Button onClick={handleUpdateBrand} disabled={updateLoading}>
+        <Button
+          onClick={handleUpdateBrand}
+          disabled={loading || !isFormValid()}
+        >
           Oppdater
         </Button>
       }
     >
       <TextField
-      label="Brand Name"
-      value={brandName}
-      onChange={(e) => setBrandName(e.target.value)}
-    />
+        label="Brand Name"
+        value={brandName}
+        onChange={(e) => {
+          setBrandName(e.target.value);
+          resetValidationErrors();
+          resetServerError(); // Clear validation errors when input changes
+        }}
+        error={Boolean(validationError)}
+      />
+      {displayError || validationError ? (
+        <ErrorHandling resource="brands" loading={loading} />
+      ) : null}
     </BasicDialog>
-  )
+  );
 };
 
 EditBrandDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
   dialogTitle: PropTypes.string.isRequired,
-  cancelButton: PropTypes.node.isRequired,
   selectedBrand: PropTypes.object.isRequired,
+  onUpdateSuccess: PropTypes.func.isRequired,
+  onUpdateFailure: PropTypes.func.isRequired,
 };
 
 export default EditBrandDialog;
