21621c762226524bb79315176f764e1dabc463c8
gekoparty <gekoparty@hotmail.com>
2023-07-18 22:35:35 +0200

    fixed bug in editBrand

--- frontend/src/hooks/useSnackBar.js
+++ frontend/src/hooks/useSnackBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { Snackbar, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
