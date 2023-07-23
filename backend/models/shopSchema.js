970296f8c73d3368f187d4825dcf9eb2741feb7b
gekoparty <gekoparty@hotmail.com>
2023-07-23 02:33:01 +0200

    change server.js to check for slug

--- backend/models/shopSchema.js
+++ backend/models/shopSchema.js
@@ -46,6 +46,9 @@ const shopSchema = new mongoose.Schema(
   }
 );
 
+shopSchema.index({ slugifiedName: 1, slugifiedLocation: 1 }, { unique: true });
+shopSchema.index({ name: 1, location: 1 }, { unique: true });
+
 const Shop = mongoose.model("Shop", shopSchema);
 
 export default Shop;
