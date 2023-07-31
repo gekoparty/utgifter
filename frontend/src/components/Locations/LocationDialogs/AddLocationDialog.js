654ddcd51ae3dd7a06096f70af78028c97f45a06
gekoparty <gekoparty@hotmail.com>
2023-07-31 02:29:27 +0200

    changed location to form

--- frontend/src/components/Locations/LocationDialogs/AddLocationDialog.js
+++ frontend/src/components/Locations/LocationDialogs/AddLocationDialog.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button, TextField, CircularProgress } from "@mui/material";
+import { Button, TextField, CircularProgress, Box } from "@mui/material";
 import PropTypes from "prop-types";
 import BasicDialog from "../../commons/BasicDialog/BasicDialog";
 import ErrorHandling from "../../commons/ErrorHandling/ErrorHandling";
 import useLocationDialog from "../UseLocation/useLocationDialog";
 
-const AddLocationDialog = ({ open,onClose, onAdd }) => {
+const AddLocationDialog = ({ open, onClose, onAdd }) => {
   const {
     locationName,
     setLocationName,
@@ -16,19 +16,20 @@ const AddLocationDialog = ({ open,onClose, onAdd }) => {
     displayError,
     validationError,
     isFormValid,
-    resetFormAndErrors, 
+    resetFormAndErrors,
   } = useLocationDialog();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  const handleAddLocation = async () => {
-    // Call the handleSaveBrand function from the hook to save the new brand
-    const success = await handleSaveLocation(onClose);
-    if (success) {
-      onAdd({ name: locationName }); // Trigger the onAdd function to show the success snackbar with the brand name
+    // Call the handleSaveLocation function from the hook to save the new location
+    if (isFormValid()) {
+      const success = await handleSaveLocation(onClose);
+      if (success) {
+        onAdd({ name: locationName }); // Trigger the onAdd function to show the success snackbar with the location name
+      }
     }
   };
-
-
 
   return (
     <BasicDialog
@@ -38,35 +39,44 @@ const AddLocationDialog = ({ open,onClose, onAdd }) => {
         onClose(); // Close the dialog after resetting the form and errors
       }}
       dialogTitle="Nytt sted"
-      confirmButton={
-        <Button onClick={handleAddLocation} disabled={loading || !isFormValid()}>
-          {loading ? <CircularProgress size={24} /> : "Lagre"}
-        </Button>
-      }
-      cancelButton={
-        <Button onClick={() => {
-          resetFormAndErrors(); // Reset the form and errors when the cancel button is clicked
-          onClose(); // Close the dialog
-        }}>
-          Cancel
-        </Button>
-      }
     >
-      <TextField
-      size="small"
-        sx={{ marginTop: 2 }}
-        label="Sted"
-        value={locationName}
-        error={Boolean(validationError)}
-        onChange={(e) => {
-          setLocationName(e.target.value);
-          resetValidationErrors();
-          resetServerError(); // Clear validation errors when input changes
-        }}
-      />
-      {displayError || validationError ? (
-        <ErrorHandling resource="locations" field="locationName" loading={loading} />
-      ) : null}
+      <form id="addLocationForm" onSubmit={handleSubmit}>
+        <TextField
+          size="small"
+          sx={{ marginTop: 2 }}
+          label="Sted"
+          value={locationName}
+          error={Boolean(validationError)}
+          onChange={(e) => {
+            setLocationName(e.target.value);
+            resetValidationErrors();
+            resetServerError(); // Clear validation errors when input changes
+          }}
+        />
+        {displayError || validationError ? (
+          <ErrorHandling
+            resource="locations"
+            field="locationName"
+            loading={loading}
+          />
+        ) : null}
+
+        <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}>
+          
+          <Button
+            onClick={() => {
+              resetFormAndErrors(); // Reset the form and errors when the cancel button is clicked
+              onClose(); // Close the dialog
+            }}
+            sx={{ marginLeft: 2 }}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" disabled={loading || !isFormValid()}>
+            {loading ? <CircularProgress size={24} /> : "Lagre"}
+          </Button>
+        </Box>
+      </form>
     </BasicDialog>
   );
 };
