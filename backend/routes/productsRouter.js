b070a89dc51a417351b99712cef04a0378d792a4
gekoparty <gekoparty@hotmail.com>
2023-08-18 20:44:13 +0200

    EditProduct works

--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
-import slugify from 'slugify'
+import slugify from "slugify";
 import Product from "../models/productSchema.js";
-import Brand from '../models/brandSchema.js';
+import Brand from "../models/brandSchema.js";
 
 const productsRouter = express.Router();
 
@@ -19,7 +19,7 @@ productsRouter.post("/", async (req, res) => {
   console.log("Request body:", JSON.stringify(req.body, null, 2));
   try {
     const { name, brands, measurementUnit } = req.body;
-    
+
     const brandIds = [];
 
     for (const brandObj of brands) {
@@ -49,12 +49,12 @@ productsRouter.post("/", async (req, res) => {
     }
 
     const existingProduct = await Product.findOne({
-      slug: slugify(name, {lower: true}),
+      slug: slugify(name, { lower: true }),
     });
 
     if (existingProduct) {
       const existingProductWithBrands = await Product.findOne({
-        slug: slugify(name, {lower: true}),
+        slug: slugify(name, { lower: true }),
         brands: { $in: brandIds },
       });
 
@@ -67,7 +67,7 @@ productsRouter.post("/", async (req, res) => {
       name,
       measurementUnit,
       brands: brandIds,
-      slug: slugify(name, {lower: true}),
+      slug: slugify(name, { lower: true }),
     });
 
     try {
@@ -76,7 +76,9 @@ productsRouter.post("/", async (req, res) => {
       const savedProduct = await product.save();
       console.log("Saved product:", savedProduct);
 
-      const populatedProduct = await Product.findById(savedProduct._id).populate("brands").exec();
+      const populatedProduct = await Product.findById(savedProduct._id)
+        .populate("brands")
+        .exec();
 
       res.status(201).json(populatedProduct);
     } catch (saveError) {
@@ -88,7 +90,6 @@ productsRouter.post("/", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
-
 
 productsRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
@@ -105,10 +106,79 @@ productsRouter.delete("/:id", async (req, res) => {
   }
 });
 
+productsRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  try {
+    const { name, brands, measurementUnit } = req.body;
+    const brandIds = [];
 
+    for (const brandObj of brands) {
+      if (typeof brandObj.name === "string") {
+        const slugifiedBrandName = slugify(brandObj.name, { lower: true });
+        const existingBrand = await Brand.findOne({
+          slug: slugifiedBrandName,
+        });
 
+        let brandId;
 
+        if (!existingBrand) {
+          const newBrand = new Brand({
+            name: brandObj.name,
+            slug: slugifiedBrandName,
+          });
 
+          const savedBrand = await newBrand.save();
+          brandId = savedBrand._id;
+        } else {
+          brandId = existingBrand._id;
+        }
+        brandIds.push(brandId);
+      }
+    }
+
+    const existingProduct = await Product.findOne({
+      _id: { $ne: id },
+      slug: slugify(name, { lower: true }),
+    });
+
+    if (existingProduct) {
+      const existingProductWithBrands = await Product.findOne({
+        _id: { $ne: id },
+        slug: slugify(name, { lower: true }),
+        brands: { $in: brandIds },
+      });
+      if (existingProductWithBrands) {
+        return res
+          .status(400)
+          .json({ message: "Duplicate product with same brands" });
+      }
+    }
+
+    const updatedProduct = {
+      name,
+      measurementUnit,
+      brands: brandIds,
+      slug: slugify(name, { lower: true }),
+    };
+
+    console.log(updatedProduct)
+
+    const result = await Product.findByIdAndUpdate(id, updatedProduct, {
+      new: true,
+      runValidators: true,
+      populate: "brands", // Populate the "brands" field
+    });
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: "Produkt ikke funnet" });
+    }
+  } catch (error) {
+    console.error("server error", error);
+    res.status(500).json({ message: "Intern server error" });
+  }
+});
 
 export default productsRouter;
-
