5b196d5e5a9e5d2c4851b484ee82dcc5d3601ea0
gekoparty <gekoparty@hotmail.com>
2023-08-26 22:54:03 +0200

    createableSelect Shop

---
+++ frontend/src/components/commons/Utils/apiUtils.js
@@ -0,0 +1,20 @@
+// api.js (or any appropriate file)
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://www.material-react-table.com"
+    : "http://localhost:3000";
+
+
+export const fetchLocations = async () => {
+    const fetchURL = new URL("/api/locations", API_URL); // Replace with your actual endpoint
+    const response = await fetch(fetchURL.href);
+    const data = await response.json();
+    return data;
+  };
+
+  export const fetchCategories = async () => {
+    const fetchURL = new URL("/api/categories", API_URL); // Replace with your actual endpoint
+    const response = await fetch(fetchURL.href);
+    const data = await response.json();
+    return data;
+  };
