9565961fb7415f5c44b5bf85a048a671bce37ea5
gekoparty <gekoparty@hotmail.com>
2024-08-01 13:32:38 +0200

    styles for create and select

---
+++ frontend/src/components/commons/Styles/SelectStyles.js
@@ -0,0 +1,33 @@
+const commonSelectStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    border: state.isFocused ? '2px solid #3f51b5' : '1px solid #ced4da',
+    boxShadow: state.isFocused ? '0 0 0 1px #3f51b5' : 'none',
+    '&:hover': {
+      border: '1px solid #3f51b5',
+    },
+    minHeight: '40px',
+    borderRadius: '4px',
+  }),
+  valueContainer: (provided) => ({
+    ...provided,
+    padding: '0 6px',
+  }),
+  input: (provided) => ({
+    ...provided,
+    margin: 0,
+    padding: 0,
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    margin: 0,
+    padding: 0,
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    margin: 0,
+    padding: 0,
+  }),
+};
+
+export default commonSelectStyles;
