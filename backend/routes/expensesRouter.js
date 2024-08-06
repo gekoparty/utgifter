e40b1f60b42be760ad5ad8f78b62ea93a94f655a
gekoparty <gekoparty@hotmail.com>
2024-08-06 20:48:18 +0200

    created put request

--- backend/routes/expensesRouter.js
+++ backend/routes/expensesRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import slugify from "slugify";
 import Expense from "../models/expenseSchema.js";
 import Product from "../models/productSchema.js";
 import Brand from "../models/brandSchema.js";
@@ -203,6 +204,112 @@ expensesRouter.post("/", async (req, res) => {
     const savedExpenses = await Expense.insertMany(expensesToSave);
 
     res.status(201).json(savedExpenses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+expensesRouter.put("/:id", async (req, res) => {
+  console.log("Received PUT request data:", req.body);
+  
+  const { id } = req.params;
+  const {
+    productName,
+    brandName,
+    measurementUnit,
+    type,
+    price,
+    purchased,
+    shopName,
+    purchaseDate,
+    registeredDate,
+    hasDiscount,
+    discountValue,
+    discountAmount,
+    quantity,
+    locationName,  // Required for creating/updating a shop
+    volume,
+    finalPrice,
+    pricePerUnit,
+  } = req.body;
+
+  try {
+    // Find or create the product
+    const product = await Product.findOne({ name: productName });
+    if (!product) {
+      return res.status(400).json({ message: "Invalid product." });
+    }
+
+    // Find or create the brand
+    let brand = await Brand.findOne({ name: brandName });
+    if (!brand) {
+      const brandSlug = slugify(brandName, { lower: true });
+      brand = new Brand({
+        name: brandName,
+        slug: brandSlug,
+      });
+      await brand.save();
+    }
+
+    // Find or create the location
+    let location = locationName
+      ? await Location.findOne({ name: locationName })
+      : null;
+    if (locationName && !location) {
+      const locationSlug = slugify(locationName, { lower: true });
+      location = new Location({
+        name: locationName,
+        slug: locationSlug,
+      });
+      await location.save();
+    }
+
+    // Find or create the shop (ensure location is included)
+    let shop = await Shop.findOne({ name: shopName, location: location?._id });
+    if (!shop) {
+      const shopSlug = slugify(shopName, { lower: true });
+      if (!location) {
+        return res.status(400).json({ message: "Location is required to create a shop." });
+      }
+      shop = new Shop({
+        name: shopName,
+        slugifiedName: shopSlug,
+        location: location._id,  // Assign the location to the shop
+      });
+      await shop.save();
+    }
+
+    // Find the expense by ID and update it
+    const updatedExpense = await Expense.findByIdAndUpdate(
+      id,
+      {
+        productName: product._id,
+        brandName: brand._id,
+        measurementUnit,
+        type,
+        price,
+        purchased,
+        shopName: shop._id,
+        purchaseDate,
+        registeredDate,
+        hasDiscount,
+        discountValue,
+        discountAmount,
+        quantity,
+        locationName: location ? location._id : null,
+        volume,
+        finalPrice,
+        pricePerUnit,
+      },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedExpense) {
+      return res.status(404).json({ message: "Expense not found." });
+    }
+
+    res.json(updatedExpense);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
