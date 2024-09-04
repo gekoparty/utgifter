c88544f9b780156a6353ab992d873ee031205fa1
gekoparty <gekoparty@hotmail.com>
2024-09-05 00:09:27 +0200

    more types

--- frontend/src/components/Products/ProductDialogs/AddProductDialog.js
+++ frontend/src/components/Products/ProductDialogs/AddProductDialog.js
@@ -20,7 +20,7 @@ const measurementUnitOptions = [
 
  // State to store selected brands
 
-const predefinedTypes = ["Matvare", "Gambling", "Gave","Ferje", "Reise", "Hår/Hud","Ferdigmat", "Brev/Pakke","Jernvare", "Elektronikk", "Bil", "Artikler", "Klær", "Verktøy", "Tobakk"]; // Add your predefined types here
+const predefinedTypes = ["Matvare", "Alkohol", "Gambling", "Bil", "Soverom", "Kjøkken", "Hage", "Datautstyr", "Gave","Ferje", "Reise", "Hår/Hud","Ferdigmat", "Brev/Pakke","Jernvare", "Elektronikk", "Bil", "Artikler", "Klær", "Verktøy", "Tobakk"]; // Add your predefined types here
 
 const AddProductDialog = ({ open, onClose, onAdd }) => {
   const {
