cec7f516d9e0b9069b23877c59387daf723a9070
gekoparty <gekoparty@hotmail.com>
2025-07-19 21:27:48 +0200

    fixed name mismatch

--- backend/routes/categoriesRouter.js
+++ backend/routes/categoriesRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Category from "../models/categorySchema.js";
+import Category from "../models/CategorySchema.js";
 import slugify from "slugify";
 
 const categoriesRouter = express.Router();
